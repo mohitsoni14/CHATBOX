baf473deeb68c5cfba36ca4f292e83927a32854b
Kshitij Jain <negokshitij@gmail.com>
2025-09-01 00:30:51 +0530

    changes applied

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { router } from './routes';
 import './index.css';
 import './sytle/globals.css';
 
+console.log('Pusher Key:', import.meta.env.VITE_PUSHER_APP_KEY);
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
